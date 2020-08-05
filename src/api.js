5a6af7435cd85c70db039decd6176b0df6b08b15
Brian Barton <brian.n.barton@gmail.com>
2020-08-05 12:00:43 -0500

    wednesday

--- src/api.js
+++ src/api.js
@@ -1,18 +1,32 @@
 module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-        .then(response => response.json());
-  },
-  postMovie: (movie) => {
-    return fetch('api/movies', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(movie),
-
-    })
-  }
+    getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json());
+    },
+    postMovie: (movie) => {
+        return fetch('api/movies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        });
+    },
+    patchMovie: (movie, id) => {
+        return fetch(`api/movies/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        });
+    },
+    deleteMovie: (id) => {
+        return fetch(`api/movies/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
 }
-
-//get post delete and patch
--- src/index.js
+++ src/index.js
@@ -10,20 +10,45 @@ sayHello('World');
  */
 const {getMovies, postMovie} = require('./api.js');
 
-getMovies().then((movies) => {
-    console.log('Here are all the movies:');
-    movies.forEach(({title, rating, id}) => {
-        console.log(`id#${id} - ${title} - rating: ${rating}`);
+const movieTitle = document.querySelector(`#title`)
+const formRating = document.querySelector(`#rating`)
+
+const insertMovie = document.querySelector(`#insert-movie`);
+
+let refreshMovies = function() {
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        insertMovie.innerHTML=``;
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+            insertMovie.innerHTML+=(`
+          <tr>
+          <td>${title}</td>
+          <td>${rating}</td>
+            <td>#button</td>
+            </tr>
+            `)
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
     });
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
-});
+}
+refreshMovies();
+//document.getElementById("search-by-title-button").addEventListener("click", postMovie);
 
 
-//user data appends to api
+const postBtn = document.querySelector('#search-by-title-button');
+postBtn.addEventListener('click', () => {
+   postMovie({title: movieTitle.value, rating: formRating.value}).then((data) => {
+       console.log(data);
+       refreshMovies()
+   })
+})
 
 
-//When the initial ajax request comes back, remove the "loading..."
+
+
+
 // // message and replace it with HTML generated from the json response
 // // your code receives 
